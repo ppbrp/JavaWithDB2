98f65e85e48c406276dcfef79fd0d71bdc44287e
ppbrp <pongboripatnioz@gmail.com>
2024-06-19 20:27:06 +0700

    Add files via upload

---
+++ ProductOrderGUI/src/productordergui/ProductOrderGUI.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package productordergui;
+
+/**
+ *
+ * @author Student
+ */
+public class ProductOrderGUI {
+    public static void main(String[] args) {
+//        CustomerForm appForm = new CustomerForm();
+//        appForm.setVisible(true);
+          LoanFormGUI loanForm = new LoanFormGUI();
+          loanForm.setVisible(true);
+    }
+    
+}
